1d3d207c18a9b0c838eef63997faf0524738366e
Chhavi Verma <chhaviverma1703@gmail.com>
2025-11-21 13:14:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import Description from './Description.jsx';
+// import Title from "./Title.jsx";
+// import ImageOFCard from './Imgofcard.jsx';
+//import Product from './product.jsx'
+// import{Title2} from "./Title.jsx";
+import ProductTab from "./ProductTab";
+
+
+
+function App() {
+ return  <ProductTab/>
+
+}
+export default App;
---
+++ src/Product.jsx
@@ -0,0 +1,15 @@
+import Title from "./Title.jsx";
+import "./product.css";
+
+function Product() {
+  return (
+    <>
+      <div className="Product">
+        <h4>Product Title</h4>
+        <p>Product Description</p>
+      </div>
+    </>
+  );
+}
+
+export default Product;
