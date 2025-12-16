1ecc17bf578f335abfd5ac0c06079f7b83777605
Chhavi Verma <chhaviverma1703@gmail.com>
2025-12-16 13:21:53 +0530

    Add product components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import "./App.css";
-<<<<<<< HEAD
-import Counter from "./Counter.jsx";
-=======
 import ProductTab from "./ProductTab";
 import Button from "./Button.jsx";
 import Form from "./Form.jsx";
->>>>>>> 891dc6a5b748882c21fd953650dcf0d6b22e3659
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <h1>States in react</h1>
-      <Counter />
-    </>
-  );
-}
-
-=======
       {/* <h2>BIG BAZAR DEALS!!!</h2>
       <ProductTab /> */}
 
@@ -27,5 +15,4 @@ function App() {
     </>
   );
 }
->>>>>>> 891dc6a5b748882c21fd953650dcf0d6b22e3659
 export default App;
